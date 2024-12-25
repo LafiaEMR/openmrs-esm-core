955b2581f556d21f5ad2af554adf9f48b2c0ab50
alok <alok.subedi@seerglobalsolutions.com>
2024-12-25 11:44:24 +0545

    Refactor: Modify test to remove My Account button

--- packages/apps/esm-primary-navigation-app/src/root.component.test.tsx
+++ packages/apps/esm-primary-navigation-app/src/root.component.test.tsx
@@ -45,20 +45,20 @@ describe('Root', () => {
   it('should display navbar with title', async () => {
     render(<Root />);
 
-    expect(screen.getByRole('button', { name: /My Account/i })).toBeInTheDocument();
+    // expect(screen.getByRole('button', { name: /My Account/i })).toBeInTheDocument();
     expect(screen.getByRole('banner', { name: /openmrs/i })).toBeInTheDocument();
     expect(screen.getByText(/mock emr/i)).toBeInTheDocument();
   });
 
-  it('should open user-menu panel', async () => {
-    const user = userEvent.setup();
+  // it('should open user-menu panel', async () => {
+  //   const user = userEvent.setup();
 
-    render(<Root />);
+  //   render(<Root />);
 
-    const userButton = screen.getByRole('button', { name: /My Account/i });
-    await user.click(userButton);
-    expect(screen.getByLabelText(/location/i)).toBeInTheDocument();
-  });
+  //   const userButton = screen.getByRole('button', { name: /My Account/i });
+  //   await user.click(userButton);
+  //   expect(screen.getByLabelText(/location/i)).toBeInTheDocument();
+  // });
 
   describe('when view is desktop', () => {
     beforeEach(() => {
