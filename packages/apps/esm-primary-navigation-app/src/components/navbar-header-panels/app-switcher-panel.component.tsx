be58e1bb9d6cb23d2d0992809ec83b29cee8ab72
alok <alok.subedi@seerglobalsolutions.com>
2025-01-10 12:27:43 +0545

    Feat: Add link to return to Lafiacore form app switcher

--- packages/apps/esm-primary-navigation-app/src/components/navbar-header-panels/app-switcher-panel.component.tsx
+++ packages/apps/esm-primary-navigation-app/src/components/navbar-header-panels/app-switcher-panel.component.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { appSwitcherLinks } from './helper/constants';
 import AppswitchArrow from '../../../public/svg/appSwitchArrow';
 import styles from './app-switcher-panel.scss';
+import { Link } from 'react-router-dom';
 
 function AppSwitcherPanel({ menuOpen }: { menuOpen: boolean }) {
   return (
@@ -11,17 +12,16 @@ function AppSwitcherPanel({ menuOpen }: { menuOpen: boolean }) {
       {menuOpen && (
         <div className={styles.menu}>
           <p className={styles.menu_item_title}>My Apps</p>
-          {appSwitcherLinks.map(({ name, Icon }, index) => (
-            //   <Link to={name}>
-            <div className={styles.menu_container}>
-              <div className={styles.menu_container2}>
-                <Icon />
-                <p className={styles.menu_item}>{name}</p>
+          {appSwitcherLinks.map(({ name, href, Icon }, index) => (
+            <Link to={href}>
+              <div className={styles.menu_container}>
+                <div className={styles.menu_container2}>
+                  <Icon />
+                  <p className={styles.menu_item}>{name}</p>
+                </div>
+                {index == 1 && <AppswitchArrow />}
               </div>
-
-              {index == 1 && <AppswitchArrow />}
-            </div>
-            //  </Link>
+            </Link>
           ))}
         </div>
       )}
